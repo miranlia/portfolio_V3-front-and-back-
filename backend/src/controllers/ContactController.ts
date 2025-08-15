a3858eb335c9906c2c02bea04c0c515ebfa4e940
Miranda Liang <mirandaliang00@gmail.com>
2025-08-15 14:15:50 -0700

    Update ContactController.ts

--- backend/src/controllers/ContactController.ts
+++ backend/src/controllers/ContactController.ts
@@ -1,1 +1,19 @@
+// backend/src/controllers/ContactController.ts
+import { Request, Response } from 'express';
+import { ContactService } from '../services/ContactService';
+import { EmailService } from '../services/EmailService';
+import { logger } from '../utils/logger';
+import type { ContactFormData } from '../types/contact';
 
+export class ContactController {
+  private contactService: ContactService;
+  private emailService: EmailService;
+
+  constructor() {
+    this.contactService = new ContactService();
+    this.emailService = new EmailService();
+  }
+
+  /**
+   * Submit contact form
+   */
